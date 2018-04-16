38807e4b921e2ecc54deef5842ae2ce34b6e8890
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-04-15 20:20:51 -0400

    add code from day two for prolog

---
+++ week-3-prolog/day-2/lists.pl
@@ -0,0 +1,17 @@
+invert([], []).
+invert([X], [X]).
+invert(Reversed, [Head|Tail]) :-
+    invert(Rest, Tail),
+    append(Rest, [Head], Reversed).
+
+smallest([X], X).
+smallest([Head|Tail], X) :- smallest(Tail, TailMin), X is min(Head,TailMin).
+
+quick_sort([],[]).
+quick_sort([H|T],Sorted):-
+	pivoting(H,T,L1,L2),quick_sort(L1,Sorted1),quick_sort(L2,Sorted2),
+	append(Sorted1,[H|Sorted2]).
+
+pivoting(H,[],[],[]).
+pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
